66fe7193368470839c1157ffb1801074442ae6a6
terrafrost <terrafrost@php.net>
2019-10-23 23:01:48 -0400

    update to test with gmp

--- test.php
+++ test.php
@@ -14,8 +14,11 @@ if (!extension_loaded('xdebug')) {
     echo 'not ';
 }
 echo "loaded\n";
-echo defined('PHP_INT_SIZE') ? PHP_INT_SIZE : 4;
-echo "\n";
+echo 'GMP is ';
+if (!extension_loaded('gmp')) {
+    echo 'not ';
+}
+echo "loaded\n";
 
 function microtime_float()
 {
@@ -23,8 +26,7 @@ function microtime_float()
     return ((float)$usec + (float)$sec);
 }
 
-define('MATH_BIGINTEGER_MODE', MATH_BIGINTEGER_MODE_INTERNAL);
-define('MATH_BIGINTEGER_OPENSSL_DISABLE', true);
+define('MATH_BIGINTEGER_MODE', MATH_BIGINTEGER_MODE_GMP);
 
 for ($i = 1; $i <= 3; $i++) {
     $start = microtime_float();
