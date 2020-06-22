73213a909793e38079dc73b8502db63aed5e438e
terrafrost <terrafrost@gmail.com>
2020-06-22 10:49:36 -0500

    ...

--- travis/setup-libssh2.sh
+++ travis/setup-libssh2.sh
@@ -3,6 +3,7 @@
 set -e
 set -x
 
+sudo apt-get update
 sudo apt-get -y install libssh2-1 libssh2-1-dev
 sudo pecl install ssh2-1.2
 echo "extension=ssh2.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
