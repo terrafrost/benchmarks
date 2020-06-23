cfea0502f87d184e2a7cb6ac8a2b4e4e48d0af99
terrafrost <terrafrost@gmail.com>
2020-06-23 11:01:30 -0500

    ...

--- test.php
+++ test.php
@@ -8,7 +8,7 @@ ssh2_auth_password($ssh, 'phpseclib', 'phpseclib');
 $sftp = ssh2_sftp($ssh);
 
 $fp = fopen('ssh2.sftp://' . intval($sftp) . '/home/phpseclib/1mb', 'w');
-fwrite($fp, str_repeat('a', 1024 * 1024));
+fwrite($fp, str_repeat('a', 10 * 1024 * 1024));
 $elapsed = microtime(true) - $start;
 echo "libssh2 / upload took $elapsed seconds\r\n";
 
@@ -27,7 +27,7 @@ $start = microtime(true);
 $sftp = new \phpseclib3\Net\SFTP('127.0.0.1');
 $sftp->login('phpseclib', 'phpseclib');
 
-$sftp->put('1mb', str_repeat('a', 1024 * 1024));
+$sftp->put('1mb', str_repeat('a', 10 * 1024 * 1024));
 $elapsed = microtime(true) - $start;
 echo "phpseclib / upload took $elapsed seconds\r\n";
 
