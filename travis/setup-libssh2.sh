39d5df0aa8edb41255f5b838bd55f4872b536a12
terrafrost <terrafrost@gmail.com>
2020-06-22 23:20:22 -0500

    ...

--- travis/setup-libssh2.sh
+++ travis/setup-libssh2.sh
@@ -5,7 +5,7 @@ set -x
 
 sudo apt-get update
 sudo apt-get -y install libssh2-1 libssh2-1-dev
-sudo pecl install ssh2-1.2
+pecl install ssh2-1.2
 echo "extension=ssh2.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 sudo useradd --create-home --base-dir /home "phpseclib"
 echo "phpseclib:phpseclib" | sudo chpasswd
